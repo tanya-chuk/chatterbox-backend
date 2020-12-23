7b5d7160d740200537b7ae4d170aa7e2b648e99e
Tanya Chuk <taosadchuk@gmail.com>
2020-12-23 19:28:46 +0300

    Мигрирует на koa

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import cors from "cors";
-import express from "express";
-import bodyParser from "body-parser";
-import { User } from "./routes/user";
+import Koa from "koa";
+import { userRouter } from "./routes/user";
+import cors from "@koa/cors";
+import koaBody from "koa-body";
 
-const app = express();
+const app = new Koa();
 const port = 9000;
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(User().url, User().router);
+app.use(koaBody());
+app.use(userRouter.routes());
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
