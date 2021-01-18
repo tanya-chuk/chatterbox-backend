6accbc60e15b6db91911def304b62c29c35f1bc8
Tanya Chuk <taosadchuk@gmail.com>
2021-01-18 15:36:38 +0300

    Добавляет возможность писать данные в базу

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import Koa from "koa";
 import { userRouter } from "./routes/user";
 import cors from "@koa/cors";
 import koaBody from "koa-body";
-import { db } from "../db";
-import { MongoClient } from "mongodb";
 
 const app = new Koa();
 const port = 9000;
@@ -11,25 +9,6 @@ const port = 9000;
 app.use(cors());
 app.use(koaBody());
 app.use(userRouter.routes());
-
-const client = new MongoClient(db.url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-client.connect((err, client) => {
-  if (err) {
-    console.log(err);
-  }
-
-  const db = client.db("test");
-  const collection = db.collection("test");
-  collection.find({}).toArray(function (err, data) {
-    if (!err) {
-      console.log(data);
-    }
-  });
-});
 
 app.listen(port, () => {
   console.log("We are live on " + port);
